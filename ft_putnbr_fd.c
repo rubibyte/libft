bf5e4d697235d668dc4178a284d8d62c24135f3d
Xavier Rodriguez-Rubi Alonso <xavirodriguezrubi@gmail.com>
2022-10-14 23:25:19 +0200

    shortened ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: xrodrigu <xrodrigu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 18:09:46 by xrodrigu          #+#    #+#             */
-/*   Updated: 2022/09/19 20:50:10 by xrodrigu         ###   ########.fr       */
+/*   Updated: 2022/10/14 23:24:02 by xrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	c;
-
 	if (n == -2147483648)
 		ft_putstr_fd("-2147483648", fd);
 	else if (n < 0)
@@ -25,10 +23,7 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(n, fd);
 	}
 	else if (n <= 9)
-	{
-		c = n + '0';
-		ft_putchar_fd(c, fd);
-	}
+		ft_putchar_fd((n + '0'), fd);
 	else
 	{
 		ft_putnbr_fd((n / 10), fd);
